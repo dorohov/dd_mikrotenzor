1d0785305bace15c229cc2a1628f43b007902ed4
Pavel Proyavin <pavel@dorohovdesign.com>
2019-06-21 17:35:11 +0300

    release

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,7 @@
     "use strict"
     $(function() {
 
-        $('.__dd').mousemove(function(e) {
-            $('.__dd__block-fo__part.is--one').css({
-                transform: 'translateX(' + e.clientX / 30 + 'px) translateY(' + e.clientY / 30 + 'px)'
-            })
-            $('.__dd__block-fo__part.is--two').css({
-                transform: 'translateX(-' + e.clientX / 30 + 'px) translateY(-' + e.clientY / 30 + 'px)'
-            }) 
-        })
+        
 
     })
 })(jQuery);
